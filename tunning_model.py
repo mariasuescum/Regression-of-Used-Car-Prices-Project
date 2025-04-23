3eecc8d8809d10b92ad6952330066e76a534d14d
César_Mercado <doncesarmercado@hotmail.com>
2025-04-23 13:23:52 +0200

    fix(tunning_model): add configuration model

--- tunning_model.py
+++ tunning_model.py
@@ -5,7 +5,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from scipy.stats import randint
 import joblib
 
-# 1. Cargar datos
+# Cargar datos
 df = pd.read_csv("data/final_dataset.csv")
 features = [
     'age', 'milage', 'accident', 'engine_hp',
@@ -14,10 +14,10 @@ features = [
 X = df[features]
 y = df['price']
 
-# 2. Dividir train/test
+# Dividir train/test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 3. Definir modelo y espacio de búsqueda
+# Definir modelo y espacio de búsqueda
 rf = RandomForestRegressor(random_state=30)
 param_distributions = {
     'n_estimators': randint(100, 500),
@@ -26,7 +26,7 @@ param_distributions = {
     'min_samples_leaf': randint(1, 20),
 }
 
-# 4. Tuning con RandomizedSearchCV
+# Tuning con RandomizedSearchCV
 random_search = RandomizedSearchCV(
     rf,
     param_distributions=param_distributions,
@@ -39,20 +39,35 @@ random_search = RandomizedSearchCV(
 )
 random_search.fit(X_train, y_train)
 
-# Calcular R² y Overfitting
-y_train_pred = random_search.predict(X_train)
+# Modelo
+model = RandomForestRegressor(
+    n_estimators=100,
+    max_depth=5,
+    min_samples_leaf=12,
+    min_samples_split=23,
+    random_state=30
+)
+
+# Entrenar el modelo
+model.fit(X_train, y_train)
+
+# Predicciones
+y_train_pred = model.predict(X_train)
+y_test_pred = model.predict(X_test)
+
+# Métricas
+mse = mean_squared_error(y_test, y_test_pred)
 r2_train = r2_score(y_train, y_train_pred)
-r2_test = r2_score(y_test, y_pred)
+r2_test = r2_score(y_test, y_test_pred)
 overfit_diff = r2_train - r2_test
 
-
-# 5. Evaluación
+# Evaluación
 best_model = random_search.best_estimator_
 y_pred = best_model.predict(X_test)
 print("Mejores hiperparámetros:", random_search.best_params_)
 print(f"R² Test: {r2_score(y_test, y_pred):.4f}")
 print(f"MSE Test: {mean_squared_error(y_test, y_pred):.2f}")
-print(f"Overfitting: {overfit_diff:.4f}")
+print(f"Overfitting (diferencia): {overfit_diff:.4f}")
 
-# 6. Guardar modelo entrenado
+# Guardar modelo entrenado
 joblib.dump(best_model, "models/random_forest_best.pkl")
